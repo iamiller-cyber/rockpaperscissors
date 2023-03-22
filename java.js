fac0eaeef10a77ee4e0847ebd97ce6c5b023de8e
Isaac <123217532+iamiller-cyber@users.noreply.github.com>
2023-03-22 16:14:35 -0400

    Create function  getPlayerSelection, create function playRound

--- java.js
+++ java.js
@@ -1,7 +1,22 @@
 let choices = ["rock", "paper", "scissors"]
 
-function getComputerChoice() {
-  let computerChoice = choices[Math.floor(Math.random()*3)]
-  return computerChoice;
+function getComputerSelection() {
+  let computerSelection = choices[Math.floor(Math.random()*3)]
+  return computerSelection;
 }
 
+
+function getPlayerSelection() { 
+  let playerSelection = (prompt("rock, paper, or scissors?").toLowerCase())
+  return playerSelection }
+
+function playRound(playerSelection, computerSelection) {
+    if (!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors')) {
+      alert('You gotta enter rock, paper, or scissors! Check ur spelling.')
+    }
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
+    alert('You win!') }
+    else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'scissors')) {
+    alert('You lose') }
+    else alert('Tie!')
+}
