9d836b85f32c22cf92e561ef208fa5c9f72d2c2a
Isaac <123217532+iamiller-cyber@users.noreply.github.com>
2023-03-23 08:27:28 -0400

    Create function game()

--- java.js
+++ java.js
@@ -1,4 +1,6 @@
 let choices = ["rock", "paper", "scissors"]
+let wins = 0
+let losses = 0
 
 function getComputerSelection() {
   let computerSelection = choices[Math.floor(Math.random()*3)]
@@ -12,11 +14,27 @@ function getPlayerSelection() {
 
 function playRound(playerSelection, computerSelection) {
     if (!(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors')) {
-      alert('You gotta enter rock, paper, or scissors! Check ur spelling.')
+    alert('You gotta enter rock, paper, or scissors! Check ur spelling.')
     }
     else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
-    alert('You win!') }
+    alert('You win - this round!') 
+    return wins++ }
     else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'scissors')) {
-    alert('You lose') }
-    else alert('Tie!')
+    alert('You lose - just this round!')
+    return losses++ }
+    else alert('Tie-d this round!')
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = getPlayerSelection()
+    let computerSelection = getComputerSelection()
+    playRound(playerSelection, computerSelection) }
+  if (wins > losses) {
+      alert('You win!')
+    }
+    else if (losses > wins) {
+      alert('You lost it all!')
+    }
+    else alert('I guess u tied')
 }
